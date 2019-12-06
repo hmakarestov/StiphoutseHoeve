36f17f12fbbef0be20f74f1e7464b2dcba63bbf3
i378011 <student@ISSDs-MacBook-Pro-7.local>
2019-12-06 10:51:31 +0100

    initial xCode project

---
+++ HorseStable/HorseStable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HorseStable
+//
+//  Created by Student on 06/12/2019.
+//  Copyright © 2019 smartmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
