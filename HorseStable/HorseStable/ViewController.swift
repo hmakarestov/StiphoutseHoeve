64e5ad611d886399c9aa64cc883eb3eeed63e648
i378011 <student@ISSDs-MacBook-Pro-7.local>
2019-12-12 13:04:27 +0100

    test with server

--- HorseStable/HorseStable/ViewController.swift
+++ HorseStable/HorseStable/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
 
     //var connectionService
     override func viewDidLoad() {
-        
+        //http://i409458core.venus.fhict.nl/horseHub link to server
         guard let url = URL(string:"http://localhost:5000/horseHub") else { return  }
         let connection = SignalRService(url: url)
     
