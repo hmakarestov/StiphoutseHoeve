819e48c64ffc1da4bb2b6f3f73e06fb9d4329487
esmee <399245@student.fontys.nl>
2020-01-09 12:09:11 +0100

    Auto stash before merge of "admin" and "origin/admin"

---
+++ HorseStable/HorseStable/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  HorseStable
+//
+//  Created by Student on 09/01/2020.
+//  Copyright © 2020 smartmobile. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    // properties
+    var Image: String
+    var Username: String
+    
+    // constructor
+    init(image: String, username: String){
+        self.Image = image
+        self.Username = username
+    }
+}
--- HorseStable/HorseStable/Users.swift
+++ HorseStable/HorseStable/Users.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class Users: UIViewController {
-    @IBOutlet weak var labelNameUser: UILabel!
+class Users: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var usersTableView: UITableView!
+    
+    var user : User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        usersTableView.delegate = self
+        usersTableView.dataSource = self
+        user = .init(image: "./Assets/anonymous user icon", username: "Stella van Sanden")
     }
     
 
@@ -27,12 +33,34 @@ class Users: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "GoToUser"{
-            let vc = segue.destination as? InteractionUser
-            vc?.nameUser = labelNameUser.text!
-        }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserCell
+        
+        cell.userName?.text = user?.Username
+        cell.imageView?.image = UIImage(named: "default profile image")
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! UserCell
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard?.instantiateViewController(withIdentifier: "ProfileVC") as? InteractionUser
+        vc?.nameUser = cell.userName.text!
+        self.present(vc!, animated: true, completion: nil)
+        //print("user is: " + cell.userName.text!)
+       // performSegue(withIdentifier: "GoToUser", sender: indexPath)
+        
+    }
+    
+    
 }
