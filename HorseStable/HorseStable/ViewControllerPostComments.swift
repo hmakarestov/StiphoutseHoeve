4b944be30a9cb4bf7a216dfb6b7db3b713d42d87
Robert <robert.acsinte@gmail.com>
2020-01-10 13:13:00 +0100

    added custom table view for comments

--- HorseStable/HorseStable/ViewControllerPostComments.swift
+++ HorseStable/HorseStable/ViewControllerPostComments.swift
@@ -1,30 +1,47 @@
-//
-//  ViewControllerPostComments.swift
-//  HorseStable
-//
-//  Created by Student on 09/01/2020.
-//  Copyright © 2020 smartmobile. All rights reserved.
-//
-
 import UIKit
 
-class ViewControllerPostComments: UIViewController {
+struct CellDataComments{
+    let userAvatar : String
+    let userName : String
+    let postDescription : String
+}
 
-    override func viewDidLoad() {
+class ViewControllerPostComments: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableViewComments: UITableView!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var userPosts = [
+        CellDataComments(userAvatar: "profile", userName: "Bork", postDescription: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."),
+        CellDataComments(userAvatar: "profile", userName: "Bork", postDescription: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."),
+        CellDataComments(userAvatar: "profile", userName: "Bork", postDescription: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."),
+        ]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userPosts.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCellComments", for: indexPath) as! CustomCellUComments
 
+        let post = userPosts[indexPath.row]
+        cell.userName?.text = post.userName
+        cell.userImg?.image = UIImage(named: post.userAvatar)
+        cell.postDescription?.text = post.postDescription
+
+        return cell
+    }
 }
+
+class CustomCellUComments : UITableViewCell{
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userImg: UIImageView!
+    @IBOutlet weak var postDescription: UITextView!
+    
+    
+}
+
