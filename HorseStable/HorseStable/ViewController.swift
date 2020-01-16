67266e049b9504eb2ec784fe061433a7d089c59c
esmee <399245@student.fontys.nl>
2020-01-16 15:04:24 +0100

    Revert "Merge branch 'user'"
    This reverts commit f65c024dbade1aa66048bea319c64084e481811c.

--- HorseStable/HorseStable/ViewController.swift
+++ HorseStable/HorseStable/ViewController.swift
@@ -13,12 +13,8 @@ class ViewController: UIViewController,UINavigationBarDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+    }
 
-        }
-    
 
-    
-    
 }
 
